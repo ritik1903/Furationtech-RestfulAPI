60a960ee369d03d4609ec74d4a433fef7207ff0c
ritik1903 <71216958+ritik1903@users.noreply.github.com>
2023-06-20 10:17:50 +0530

    Created model successfully

---
+++ models/itemModel.js
@@ -0,0 +1,30 @@
+const mongoose  = require('mongoose');
+
+const itemSchema = mongoose.Schema(
+    {
+        name:{
+            type : String,
+            required : [true, "Please enter a item name"]
+        },
+        quantity : {
+            type : Number,
+            required : true,
+            default : 0
+        },
+        price : {
+            type : String,
+            required : true,
+        }, 
+        image : {
+            type : String,
+            required : false,
+        }
+    },
+    {
+        timestamps : true
+    }
+)
+
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
--- server.js
+++ server.js
@@ -1,11 +1,38 @@
 const express = require('express');
-const app = express()
+const mongoose = require('mongoose');
+const port = 8000;
+const Item = require('./models/itemModel');
+const app = express();
+
+//JSON middleware
+app.use(express.json())
 
 //Routes
 app.get('/', (req, res) => {
     res.send('Hello Node API')
 })
 
-app.listen(8000, () => {
-    console.log(('node Api app is running on the port 8000'))
+app.get('/item', (req, res) => {
+    res.send('Choose an item')
 })
+
+app.post('/product', async(req, res) => {
+    try{
+        const item = await Item.create(req.body)
+        res.status(200).json(item);
+    }catch(error){
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+mongoose.connect('mongodb+srv://pawarsubham438:lzIXGQLrzV90Krpz@cluster6.3luznbl.mongodb.net/mernstackapi?retryWrites=true&w=majority')
+.then(() => {
+    console.log('Connected to MongoDB')
+    app.listen(8000, () => {
+        console.log('NodeAPI app is running on the port::', port)
+    });
+}).catch(() => {
+    console.log(error)
+})
+
